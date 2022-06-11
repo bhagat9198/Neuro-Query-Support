f361f83c8d07eb725f0cfcb4e96bd109c1dae590
Bhagat Singh <bhagat@Bhagats-MacBook-Pro.local>
2022-06-11 22:56:35 +0530

    merge

---
+++ frontend/src/store/actions/auth.js
@@ -0,0 +1,17 @@
+export const USER_DETAILS = 'USER_DETAILS';
+
+export const signup = ({  }) => {
+  return async (dispatch, getState) => {
+    
+  }
+}
+export const signin = ({  }) => {
+  return async (dispatch, getState) => {
+
+  }
+}
+export const getUserDetails = ({  }) => {
+  return async (dispatch, getState) => {
+
+  }
+}
