c048a4c53610a8543280023b259883b6011722d2
atulsingh-99 <atul.singh@getfleek.app>
2022-06-12 08:38:49 +0530

    queryForm modal  added

--- frontend/src/components/Common/QueryForm/QueryForm.js
+++ frontend/src/components/Common/QueryForm/QueryForm.js
@@ -8,113 +8,131 @@ const selectData = [
   { value: 1, label: "JavaScript" },
   { value: 2, label: "Python" },
 ];
-function QueryForm() {
+function QueryForm(props) {
   const [summary, setSummary] = useState();
   const [category, setCategory] = useState();
   const [priority, setPriority] = useState();
   const [description, setDescription] = useState();
   const [assignedTo, setAssignedTo] = useState();
+  const [file, setFile] = useState();
 
   return (
-    <div className={styles.backDrop}>
-      <div className={styles.cardContainer}>
-        <form>
-          <div className={styles.innerDiv}>
-            <div className={styles.heading}>
-              <h3>Create Query</h3>
+    <div className={styles.cardContainer}>
+      <form>
+        <div className={styles.innerDiv}>
+          <div className={styles.heading}>
+            <h3>Create Query</h3>
+          </div>
+          <div className={styles.topicSection}>
+            <div className={styles.topic}>
+              <span>Topic</span>
             </div>
-            <div className={styles.topicSection}>
-              <div className={styles.topic}>
-                <span>Topic</span>
-              </div>
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
-                  Select a category
-                </span>
-                <UISelect
-                  label="Category"
-                  data={selectData}
-                  value={category}
-                  setValue={setCategory}
-                />
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  marinTop: "5px",
-                }}
-              >
-                <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
-                  Select the preferred mentor
-                </span>
-                <UISelect
-                  sx={{ margin: 0 }}
-                  label="Assign To"
-                  value={assignedTo}
-                  data={[
-                    { value: 1, label: "teacher1" },
-                    { value: 2, label: "teacher2" },
-                  ]}
-                  setValue={setAssignedTo}
-                />
-              </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
+                Select a category
+              </span>
+              <UISelect
+                label="Category"
+                data={selectData}
+                value={category}
+                setValue={setCategory}
+              />
             </div>
             <div
               style={{
-                margin: "10px",
-                width: "100%",
-                borderBottom: "2px solid #dedede",
+                display: "flex",
+                flexDirection: "column",
+                marinTop: "5px",
               }}
-            ></div>
-
-            <div className={styles.summarySection}>
-              <div className={styles.topic}>
-                <span>Summary</span>
-              </div>
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
-                  Enter a Summary
-                </span>
-                <InputField
-                  label="Summary"
-                  type="text"
-                  value={summary}
-                  onChange={(e) => setSummary(e.target.value)}
-                />
-              </div>
-
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
-                  Please provide the details of the query
-                </span>
-                <InputField
-                  label="Description"
-                  type="textarea"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                />
-              </div>
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
-                  Select a priority
-                </span>
-                <UISelect
-                  label="Priority"
-                  value={priority}
-                  data={[
-                    { value: 0, label: "low" },
-                    { value: 1, label: "mediocre" },
-                    { value: 2, label: "High" },
-                  ]}
-                  setValue={setPriority}
-                />
-              </div>
+            >
+              <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
+                Select the preferred mentor
+              </span>
+              <UISelect
+                sx={{ margin: 0 }}
+                label="Assign To"
+                value={assignedTo}
+                data={[
+                  { value: 1, label: "teacher1" },
+                  { value: 2, label: "teacher2" },
+                ]}
+                setValue={setAssignedTo}
+              />
             </div>
-            <UIButton type="primary">Create Query</UIButton>
           </div>
-        </form>
-      </div>
+          <div
+            style={{
+              margin: "10px",
+              width: "100%",
+              borderBottom: "2px solid #dedede",
+            }}
+          ></div>
+
+          <div className={styles.summarySection}>
+            <div className={styles.topic}>
+              <span>Details</span>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
+                Enter a Summary
+              </span>
+              <InputField
+                label="Summary"
+                type="text"
+                value={summary}
+                onChange={(e) => setSummary(e.target.value)}
+              />
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
+                Please provide the Description
+              </span>
+              <InputField
+                label="Description"
+                type="textarea"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
+                Attactments (Optional)
+              </span>
+              <InputField
+                label="File Upload"
+                type="file"
+                value={file}
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </div>
+
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <span style={{ color: "#7e8e9f", fontWeight: "500" }}>
+                Select a priority
+              </span>
+              <UISelect
+                label="Priority"
+                value={priority}
+                data={[
+                  { value: 0, label: "low" },
+                  { value: 1, label: "mediocre" },
+                  { value: 2, label: "High" },
+                ]}
+                setValue={setPriority}
+              />
+            </div>
+          </div>
+          <UIButton
+            onClick={() => {
+              props.setModal(false);
+            }}
+            type="primary"
+          >
+            Create Query
+          </UIButton>
+        </div>
+      </form>
     </div>
   );
 }
--- frontend/src/views/student/QueryPage/QueryPage.js
+++ frontend/src/views/student/QueryPage/QueryPage.js
@@ -1,3 +1,4 @@
+import { Modal } from "@mui/material";
 import React from "react";
 import BodyLayout from "../../../components/BodyLayout";
 import QueryCardItem from "../../../components/Common/QueryCard/QueryCardItem";
@@ -6,12 +7,21 @@ import QueryHeader from "../../../components/Common/QueryHeader/QueryHeader";
 import QuerySummary from "../../../components/Common/QuerySummary/QuerySummary";
 import styles from "./QueryPage.module.css";
 function QueryPage() {
+  const [showModal, setShowModal] = React.useState(false);
+
   return (
     <BodyLayout>
-      <QueryHeader />
-      <QueryForm />
+      <QueryHeader setModal={setShowModal} />
+      {/* <QueryForm /> */}
+      <Modal
+        sx={{ mt: 5 }}
+        open={showModal}
+        onClose={() => setShowModal(false)}
+      >
+        <QueryForm setModal={setShowModal} />
+      </Modal>
 
-      {/* <div className={styles.wrapperDiv}>
+      <div className={styles.wrapperDiv}>
         <div className={styles.QueryList}>
           <QueryCardItem />
           <QueryCardItem />
@@ -30,7 +40,7 @@ function QueryPage() {
             status=""
           />
         </div>
-      </div> */}
+      </div>
     </BodyLayout>
   );
 }
